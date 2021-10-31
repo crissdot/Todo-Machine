04772e591b1ecbbdd61f6719c5cf551fcd57cd7c
crissdot <cris-2-11-2001@hotmail.com>
2021-10-31 17:57:31 -0600

    add className in TodoItem

--- src/App.js
+++ src/App.js
@@ -18,7 +18,13 @@ function App() {
       <TodoSearch />
 
       <TodoList>
-        {todos.map(todo => (<TodoItem key={todo.text} text={todo.text} />))}
+        {todos.map(todo => (
+          <TodoItem 
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed} 
+          />
+        ))}
       </TodoList>
 
       <CreateTodoButton />
