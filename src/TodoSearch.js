3ae399f9073d73b6febc9232799e21fb2b8ba235
crissdot <cris-2-11-2001@hotmail.com>
2021-10-31 18:37:13 -0600

    add useState in TodoSearch

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
 function TodoSearch() {
-    const onSearchValueChange = (e) => {
-        console.log(e.target.value);
+    const [searchValue, setSearchValue] = React.useState('');
+
+    const onSearchValueChange = (event) => {
+        console.log(event.target.value);
+        setSearchValue(event.target.value);
     };
 
-    return (
+    return [
         <input 
             placeholder="Busca un todo" 
+            value={searchValue}
             onChange={onSearchValueChange}
-        />
-    );
+        />,
+        <p>{searchValue}</p>
+    ];
 }
 
 export { TodoSearch };
