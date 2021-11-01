d30ac54c6d55834318b72cc7884bea57c54c6763
crissdot <cris-2-11-2001@hotmail.com>
2021-10-31 18:10:33 -0600

    add event in TodoItem

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,15 +1,28 @@
 import React from 'react';
 
 function TodoItem(props) {
+    const onComplete = () => {
+        alert('La tarea ' + props.text + ' ha sido completada');
+    };
+    const onDelete = () => {
+        alert('La tarea ' + props.text + ' ha sido eliminada');
+    }
+
     return (
         <li className="TodoItem">
-            <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>
+            <span 
+                className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+                onClick={onComplete}
+            >
                 √
             </span>
             <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
                 {props.text}
             </p>
-            <span className="Icon Icon-delete">
+            <span 
+                className="Icon Icon-delete"
+                onClick={onDelete}
+            >
                 X
             </span>
         </li>
