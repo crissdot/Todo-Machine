57805d5d567cbc72cde7b48774371257f5032892
crissdot <cris-2-11-2001@hotmail.com>
2021-10-31 19:00:20 -0600

    add useState in parent component

--- src/App.js
+++ src/App.js
@@ -5,17 +5,29 @@ import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 
-const todos = [
+const defaultTodos = [
   { text: 'Tarea1', completed: false },
   { text: 'Tarea2', completed: false },
   { text: 'Tarea3', completed: false },
 ];
 
 function App() {
+  const [searchValue, setSearchValue] = React.useState('');
+  const [todos, setTodos] = React.useState(defaultTodos);
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+
   return (
     <React.Fragment>
-      <TodoCounter />
-      <TodoSearch />
+      <TodoCounter
+        total={totalTodos}
+        completed={completedTodos}
+      />
+      <TodoSearch 
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
 
       <TodoList>
         {todos.map(todo => (
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,21 +1,18 @@
 import React from 'react';
 
-function TodoSearch() {
-    const [searchValue, setSearchValue] = React.useState('');
-
+function TodoSearch({ searchValue, setSearchValue }) {
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
         setSearchValue(event.target.value);
     };
 
-    return [
+    return (
         <input 
             placeholder="Busca un todo" 
             value={searchValue}
             onChange={onSearchValueChange}
-        />,
-        <p>{searchValue}</p>
-    ];
+        />
+    );
 }
 
 export { TodoSearch };
