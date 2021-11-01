664b31200a6c39ee1536f4b755d5ad364627cc1d
crissdot <cris-2-11-2001@hotmail.com>
2021-10-31 18:15:38 -0600

    add event in TodoSearch

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
 function TodoSearch() {
+    const onSearchValueChange = (e) => {
+        console.log(e.target.value);
+    };
+
     return (
-        <input placeholder="Busca un todo" />
+        <input 
+            placeholder="Busca un todo" 
+            onChange={onSearchValueChange}
+        />
     );
 }
 
