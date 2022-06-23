8b98fec15f27ad5211901a56e1c52cd9f7f11cec
crissdot <cris-2-11-2001@hotmail.com>
2022-06-23 16:27:42 -0500

    refactor using component composition

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,71 @@
 import React from 'react';
-import { TodoProvider } from '../TodoContext';
-import { AppUI } from './AppUI';
+import { useTodos } from './useTodos';
+import { TodoHeader } from '../TodoHeader';
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from '../TodoSearch';
+import { TodoList } from '../TodoList';
+import { TodoItem } from '../TodoItem';
+import { TodoForm } from '../TodoForm';
+import { CreateTodoButton } from '../CreateTodoButton';
+import { Modal } from '../Modal';
 
 function App() {
+    const {
+        error, 
+        loading, 
+        searchedTodos, 
+        completeTodo, 
+        deleteTodo, 
+        openModal, 
+        setOpenModal, 
+        totalTodos,
+        completedTodos,
+        searchValue,
+        setSearchValue,
+        addTodo,
+    } = useTodos();
+
     return (
-        <TodoProvider>
-            <AppUI />
-        </TodoProvider>
+        <React.Fragment>
+            <TodoHeader>
+                <TodoCounter
+                    totalTodos={totalTodos}
+                    completedTodos={completedTodos}
+                />
+                <TodoSearch
+                    searchValue={searchValue}
+                    setSearchValue={setSearchValue}
+                />
+            </TodoHeader>
+
+            <TodoList>
+                {error && <p>No pudimos cargar, F</p>}
+                {loading && <p>Estamos cargando, por favor espere</p>}
+                {!loading && !!searchValue.length && !searchedTodos.length && <p>No pudimos encontrar lo que buscabas :(</p>}
+                {!loading && !searchValue.length && !searchedTodos.length && <p>Ups, luce muy vacío, ¿por qué no creamos un TODO?</p>}
+
+                {!loading && searchedTodos.map(todo => (
+                    <TodoItem
+                        key={todo.text}
+                        text={todo.text}
+                        completed={todo.completed}
+                        onComplete={() => completeTodo(todo.text)}
+                        onDelete={() => deleteTodo(todo.text)}
+                    />
+                ))}
+            </TodoList>
+
+            {!!openModal && (
+                <Modal>
+                    <TodoForm
+                        addTodo={addTodo}
+                        setOpenModal={setOpenModal}
+                    />
+                </Modal>
+            )}
+
+            <CreateTodoButton setOpenModal={setOpenModal} />
+        </React.Fragment>
     );
 }
 
